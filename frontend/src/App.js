e84812f6f4bd5198cb03e2f63af3709cb1a5387d
ashwin <ashwinam31@gmail.com>
2022-03-22 19:58:09 +0530

    reset the file directory

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SignInForm from './Components/SignInForm/SignInForm';
+
+function App() {
+  return (
+    <div>
+      <SignInForm/>
+    </div>
+  )
+}
+
+export default App;
